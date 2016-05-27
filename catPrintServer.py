75e2e887ed0b47ca5ceb9c292fb4b4e13b6c7228
slsp8752 <slsp8752@colorado.edu>
2016-05-27 14:55:30 -0600

    Added label to MAC address location

--- catPrintServer.py
+++ catPrintServer.py
@@ -17,13 +17,13 @@ def arp_display(pkt):
 	if pkt[ARP].op == 1: #who-has (request)
         if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
             print "ARP received"
-            if pkt[ARP].hwsrc == 'a0:02:dc:7d:4e:dc': # Gatorade Button
+            if pkt[ARP].hwsrc == 'a0:02:dc:7d:4e:dc': # Put your button's MAC address here
 				print "Choosing Cat"
 				catfname = random.choice(nohidden(os.getcwd() + "/cats"))
                 print "Printing Cat!"
                 os.system("lpr -r {}".format("cats/" + catfname))
                 print(catfname)
-                r = requests.get("http://thecatapi.com/api/images/get?format=src&api_key=NTUwNzY&size=full&type=jpg")
+                r = requests.get("http://thecatapi.com/api/images/get?format=src&size=full&type=jpg")
 		urllib.urlretrieve(r.url, "cats/" + catfname)
                 print "Cat Replaced"
 
