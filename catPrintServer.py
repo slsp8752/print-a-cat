352781f5b4ad46eb800845cc0904d99d391d3394
slsp8752 <slsp8752@colorado.edu>
2016-05-27 13:49:28 -0600

    .DS_Store no longer is counted as a printable file

--- catPrintServer.py
+++ catPrintServer.py
@@ -14,12 +14,12 @@ def nohidden(path):
 	return files
 			
 def arp_display(pkt):
-    print "Choosing Cat"
-    catfname = random.choice(nohidden(os.getcwd() + "/cats"))
-    if pkt[ARP].op == 1: #who-has (request)
+	if pkt[ARP].op == 1: #who-has (request)
         if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
             print "ARP received"
             if pkt[ARP].hwsrc == 'a0:02:dc:7d:4e:dc': # Gatorade Button
+				print "Choosing Cat"
+				catfname = random.choice(nohidden(os.getcwd() + "/cats"))
                 print "Printing Cat!"
                 os.system("lpr -r {}".format("cats/" + catfname))
                 print(catfname)
