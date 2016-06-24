a4ce68ba9fb47988873ac0a99b1bb5eab9cc62df
slsp8752 <slsp8752@colorado.edu>
2016-06-24 14:12:50 -0600

    Added failsafe for empty cat buffer

--- catPrintServer.py
+++ catPrintServer.py
@@ -4,29 +4,35 @@ import requests
 import urllib
 import random
 import os
+import time
 from scapy.all import *
 
 api_key = ""
 
 def nohidden(path):
 	files = os.listdir(path)
-    for f in files:
-        if f.startswith('.'):
+	if(files) == [] or files == ['.DS_Store']:
+		r = requests.get("http://thecatapi.com/api/images/get?format=src&size=full&type=jpg&api_key=" + api_key)
+		urllib.urlretrieve(r.url, "cats/" + "kitty.jpg")
+
+	files = os.listdir(path)
+	for f in files:
+		if f.startswith('.'):
 			files.remove(f)
 	return files
 			
 def arp_display(pkt):
 	if pkt[ARP].op == 1: #who-has (request)
-        if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
-            print "ARP received"
-            if pkt[ARP].hwsrc == 'a0:02:dc:7d:4e:dc': # Put your button's MAC address here
+		if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+			print "ARP received"
+			if pkt[ARP].hwsrc == 'a0:02:dc:7d:4e:dc': # Put your button's MAC address here
 				print "Choosing Cat"
 				catfname = random.choice(nohidden(os.getcwd() + "/cats"))
-                print "Printing Cat!"
-                os.system("lpr -r {}".format("cats/" + catfname))
-                print(catfname)
-                r = requests.get("http://thecatapi.com/api/images/get?format=src&size=full&type=jpg&api_key=" + api_key)
-		urllib.urlretrieve(r.url, "cats/" + catfname)
+				print "Printing Cat!"
+				os.system("lpr -r {}".format("cats/" + catfname))
+				print(catfname)
+				r = requests.get("http://thecatapi.com/api/images/get?format=src&size=full&type=jpg&api_key=" + api_key)
+				urllib.urlretrieve(r.url, "cats/" + catfname)
                 print "Cat Replaced"
 
 print sniff(prn=arp_display, filter="arp", store=0)
